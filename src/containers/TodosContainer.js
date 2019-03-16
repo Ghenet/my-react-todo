a2d84485d3d1c0b020835d54267a539dd0f181a9
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-03-15 18:09:29 -0700

    Added css and bg image to the index page

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class TodosContainer extends Component {
   render() {
     return (
-      <h2>
-        I am the TodosContainer page
-      </h2>
+      <div className='todosContainer'>
+        <h2>I am the TodosContainer page</h2>
+      </div>
     );
   }
 }
