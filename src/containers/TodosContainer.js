104c2572b0d4edf69292122cf7e726f16e70d574
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-03-15 19:31:25 -0700

    added a CreateTodoForm component and a create todo handlers

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import TodoModel from '../models/Todo';
 import Todos from '../components/Todos';
-
+import CreateTodoForm from '../components/CreateTodoForm'
 class TodosContainer extends Component {
       state ={
         todos:[]
@@ -17,10 +17,22 @@ class TodosContainer extends Component {
           })
         })
       }
+      createTodo = (todo) =>{
+        let newTodo = {
+          body: todo,
+          completed: false
+        }
+        TodoModel.create(newTodo).then((res)=>{
+          let todos = this.state.todos
+          let newTodos = todos.push(res.data)
+            this.setState({newTodos})
+        })
+      }
   render() {
     return (
       <div className='todosComponent'>
         <Todos todos={this.state.todos} />
+        <CreateTodoForm createTodo={this.createtodo} />
       </div>
     );
   }
